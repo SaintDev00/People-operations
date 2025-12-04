3ca4b68a9d2b2260ad2f6f7c75156efa720da6e3
Saint Dev <jqydjdj@gmail.com>
2025-12-04 01:26:24 -0500

    Refactor validate_credentials function for clarity

--- Users.py
+++ Users.py
@@ -1,29 +1,20 @@
 import csv
-
 import os
 
 def validate_credentials(user, password):
-        #Validates credentials against the csv file
-           
-        if not os.path.exists('users.csv'):
-        #Create file with default admin user
-            
-            with open ('users.csv', 'w', newline='', encoding='utf_8') as f:
-                
-                writer = csv.writer(f)
-                
-                writer.writerow(['user', 'password', 'rol'])
-                
-                writer.writerow(['admin', 'admin123', 'administrator'])
-                
-                
-        with open('usuarios.csv', 'r', encoding='utf_8') as f:
-            
-            reader = csv.DictReader(f)
-            
-            for row in reader:
-                
-                if row['user'] == user and row['password'] == password:
-                    
-                    
-                    return False
+    """Validates credentials against the csv file"""
+    
+    if not os.path.exists('users.csv'):
+        # Create file with default admin user
+        with open('users.csv', 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(['user', 'password', 'rol'])
+            writer.writerow(['admin', 'admin123', 'administrator'])
+    
+    with open('users.csv', 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row['user'] == user and row['password'] == password:
+                return True
+    
+    return False
